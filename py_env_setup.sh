b8b3cf03f65298f93429cf5981fe5864c3432d0f
Lebogang PETSOANE <lpetsoan@c4r16s9.wethinkcode.co.za>
2019-11-26 10:11:38 +0200

    Im tired of running setup for each pc i move to.

---
+++ py_env_setup.sh
@@ -0,0 +1,11 @@
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Tolsadus/42homebrewfix/master/install.sh)"
+
+source ~/.zshrc
+
+brew install python3
+
+brew link python3
+
+brew install virtualenv
+
+
